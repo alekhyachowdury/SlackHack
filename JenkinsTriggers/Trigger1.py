c09c3ce7c0e8ce6bfd535ce092aaf476bb0db4b9
alekhyachowdury <44283926+alekhyachowdury@users.noreply.github.com>
2020-07-09 21:50:29 +0200

    Create Trigger1.py
    iniitial commit

---
+++ JenkinsTriggers/Trigger1.py
@@ -0,0 +1,15 @@
+import jenkins
+
+def Trigger(request):
+    JENKINS_URL = "http://35.228.157.92:8080"
+    JENKINS_USERNAME = "mindbenders"
+    JENKINS_PASSWORD = "password@1"
+
+    parameter = request.args.get('parameter')
+    print(parameter)
+    jenkins_server = jenkins.Jenkins(JENKINS_URL, username=JENKINS_USERNAME, password=JENKINS_PASSWORD)
+    if parameter == 'report':
+        print('invoke jenkins')
+        jenkins_server.build_job('Job1', parameters=None, token='job1')
+        print('jenkins invoked')
+    return 'Success'
